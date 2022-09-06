ec82d81fdcea04ca2fc783df1c7aa5ad34148e63
Łukasz <83137313+Fidek-creator@users.noreply.github.com>
2022-09-07 00:26:42 +0200

    done some practise

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -1,40 +1,27 @@
 <script setup>
+
 import { ref } from 'vue'
 
 defineProps({
   msg: String
 })
-
+var awesome = ref(true)
 const count = ref(0)
 </script>
 
 <template>
+<section>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="flex flex-col space-y-4">
+    <button type="button" class="bg-red-800 mt-6" @click="count++">count is {{ count }}</button>
+    
+    <button type="button" class="bg-blue-800" @click="awesome = !awesome">Click</button>
+    <h1 v-if="awesome">Beacuse programming is my biggest passion!</h1>
+    <h1 v-else>Just joking, I hate it 😢</h1>
+    <h2 v-if="count > 10">You clicked more than 10 times!</h2>
+    <h2 v-else>You haven't clicked more than 10 times!</h2>    
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</section>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
