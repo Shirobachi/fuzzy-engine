1a5e1f0f07705b6fce38bb6b803007a2a6ce8e10
Łukasz <83137313+Fidek-creator@users.noreply.github.com>
2022-09-08 21:52:46 +0200

    some fun with v-...

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -21,7 +21,48 @@ const count = ref(0)
     <h1 v-else>Just joking, I hate it 😢</h1>
     <h2 v-if="count > 10">You clicked more than 10 times!</h2>
     <h2 v-else>You haven't clicked more than 10 times!</h2>    
+
+  <div>
+      <h2>Sign up</h2>
+      
+      <p>
+        <input for="email" type="email" placeholder="email" required v-model="email" @blur="validateEmail" />
+      </p>
+      <p class="text-red-500" v-if="email.length == 0">E-mail cant be empty</p>
+      <p>
+        <input type="password"  v-model="password" placeholder="password" />
+      </p>
+      <p class="error-message" v-show="password.length < 6">The password must be at least 6 characters</p>
+  </div>
+  <div>
+    <p v-if="active">Using v-if</p>
+    <p v-show="active">Using v-show</p>
+    <button @click="active = !active">Toggle active</button>
+  </div>
+
   </div>
 </section>
 </template>
+
+<script>
+  export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      active: false,
+    }
+  },
+/*   methods: {
+    validateEmail(email) {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+        this.msg['email'] = 'Please enter a valid email address';
+    } else {
+        this.msg['email'] = '';
+    }
+    },
+  }, */
+  
+}
+</script>
 
